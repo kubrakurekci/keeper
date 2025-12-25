40eea90489069815874ac8e41d7090d3353dd90d
Kubra Kurekci <kubra.kurekci@gmail.com>
2025-12-25 15:21:46 +0300

    Add notes data and map notes to Note component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((notes) => (<Note key={notes.key} title={notes.title} content={notes.content} />))}
       <Footer />
     </div>
   );
